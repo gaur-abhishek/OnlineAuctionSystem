be3867d7abbe7578d0d3df49de001d79b2fca9bc
gaur-abhishek <gaur2195@gmail.com>
2018-04-03 09:59:10 +0530

    Added project files

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+      <div class="prepend-4 span-20">
+        <span id="notLink">Copyright 2011</span> |
+		<a id="notLink" href="#" title="Home">Home</a> |
+        <a id="notLink" href="#" title="About Us">About us</a> |
+		<a id="notLink" href="#" title="Contact Us">Contact us</a> |
+        <a href="#" id="notLink" title="Advertise with Us">Advertise with Us</a> |
+        <a href="#" id="notLink">Privacy Policy</a> 
+      </div>
+</div>
+<div id="tech7">
+	<div class="prepend-8 span-20">Powered By : <a href="http://www.7techsolutions.com" title="7Tech Soultions - Powered by Technology, Driven by Ethics">7Tech Solutions</a>
+	</div>
+</div>
